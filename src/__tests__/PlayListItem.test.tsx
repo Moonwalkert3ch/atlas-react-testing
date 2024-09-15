44fd8c7be938ac7019e7716a8bb1045963bd9799
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-15 17:48:21 -0500

    creted snapshot of playlistitems

--- src/__tests__/PlayListItem.test.tsx
+++ src/__tests__/PlayListItem.test.tsx
@@ -1,5 +1,30 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
-import { PlayListItem } from "../components/PlayListItem";
+import { render } from '@testing-library/react';
+import { expect, test, vi } from 'vitest';
+import { PlayListItem } from '../components/PlayListItem';
 
-test
+const onClick = vi.fn();
+
+test('PlayListItem renders correctly when current', () => {
+  const { container } = render(
+    <PlayListItem 
+      title='Painted in Blue' 
+      artist='Soul Canvas' 
+      duration='5:55' 
+      onClick={onClick} 
+    />
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test('PlayListItem renders correctly when not current', () => {
+  const { container } = render(
+    <PlayListItem 
+      title='Tidal Drift' 
+      artist='Echoes of the Sea' 
+      duration='8:02' 
+      isCurrent={false} 
+      onClick={onClick} 
+    />
+  );
+  expect(container).toMatchSnapshot();
+});
