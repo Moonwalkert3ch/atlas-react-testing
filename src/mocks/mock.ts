b5d21b2a933c1477eff1336a29ab16235a1b6281
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-15 20:28:34 -0500

    corrected input on mock

--- src/mocks/mock.ts
+++ src/mocks/mock.ts
@@ -2,19 +2,21 @@ import { setupServer } from "msw/node";
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("https://api.example.com/api/v1/data", () => {
+  http.get("https://raw.githubusercontent.com/atlas-jswank/atlas-music-player-api/main/playlist", () => {
       return HttpResponse.json([
        {
          id: 1,
-         title: "MoonWalker Music",
-         artist: "chris brown",
-         duration: "4:44"
+         title: "Test MoonWalker Music",
+         artist: "Test Artist 1",
+         duration: "4:44",
+         cover: "https://example.com/cover1.jpg"
        },
        {
          id: 2,
-         title: "The Bradford Hour",
-         artist: "jaheem",
-         duration: "11:11"
+         title: "Test Bradford Hour",
+         artist: "Test Artist 2",
+         duration: "11:11",
+          cover: "https://example.com/cover2.jpg"
        }
       ]);
     }
